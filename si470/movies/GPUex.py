48f8834321ba9956e857efff00f51193c8fb72ce
Steinshark <everettperson@gmail.com>
2022-02-10 14:51:47 -0500

    Updates to code

--- si470/movies/GPUex.py
+++ si470/movies/GPUex.py
@@ -31,6 +31,16 @@ datasets = {    'small'     :  {'movies' : join('ml-latest-small','movies.csv') 
 
 dataframes= {   'small'     :  {'movies' : None, 'ratings' :  None, 'tags' : None},
                 'large'     :  {'movies' : None, 'ratings' :  None, 'tags' : None}}
+################################################################################
+#                           Read into DataFrame
+################################################################################
+for size in datasets:
+    for set in datasets[size]:
+        if not size == 'large' and set == 'ratings':
+            dataframes[size][set]   = read_csv(datasets[size][set],sep=',')
+
+
+
 
 ################################################################################
 #                           Read into DataFrame
@@ -39,8 +49,25 @@ dataframes= {   'small'     :  {'movies' : None, 'ratings' :  None, 'tags' : Non
 t_32 = np.int64
 t_64 = np.int64
 f_16 = np.float16
-df = pd.read_csv(datasets['large']['ratings'],sep = ',',dtype={'userId':t_32,'movieId':t_64,'rating':f_16})
-printc(f"{df.head()}\n\n",TAN)
+df                              = pd.read_csv(  datasets['large']['ratings'],   sep = ',',dtype={'userId':t_32,'movieId':t_64,'rating':f_16})
+
+################################################################################
+#                           Gather constants
+################################################################################
+
+n_movies                        = int(  len(    dataframes['large'][set]        ['movieId'] ))
+n_users                         = int(          dataframes['large']['ratings']  [-1]        )
+
+################################################################################
+#                           Show Data
+################################################################################
+
+printc(f"DATASET: ")
+printc(f"\t'ratings' looks like:\n{df.head(3)}...\n{df.tail(3)}\n\n",TAN)
+printc(f"\t'movies' looks like:\n{dataframes['large']['movies'].head(3)}...\n{dataframes['large']['movies'].tail(3)}\n\n",TAN)
+
+printc(f"\tnumber of users: {n_users}",TAN)
+printc(f"\tnumber of movies: {n_movies}\n\n",TAN)
 
 ################################################################################
 #                           Build Matrix indices and values
@@ -70,5 +97,5 @@ printc(f"\tindices: {index.shape}\n\tvalue: {value.shape}",GREEN)
 matrix = tf.sparse.SparseTensor(indices=index,values=value,dense_shape=[matrix_y.shape[0],matrix_x.shape[0]])
 
 ################################################################################
-#                           Define function for Euclidean
+#                           Define a dictonary to map neighbors
 ###############################################################################
