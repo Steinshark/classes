df6a69c1f3d4e46e1c6ef646d3b8b8e80f910565
Steinshark <everettperson@gmail.com>
2022-02-09 23:04:15 -0500

    asdf

--- si486i/blockchainCode/BlockTools.py
+++ si486i/blockchainCode/BlockTools.py
@@ -131,6 +131,8 @@ def check_fields(block,allowed_versions=[0],allowed_hashes=[''],trust=False):
     # Ensure 'prev_hash' field checks out
     elif (not 'prev_hash' in block) or\
          (not block['prev_hash'] in allowed_hashes):
+        print(block['prev_hash'] in allowed_hashes)
+        input() 
         print("bad hash")
         return False
 
