bbe21db341da6f6c902617b04b5b4ab17756ef39
Everett Stenberg <m226252@usna.edu>
2022-02-10 09:12:30 -0500

    err

--- si486i/blockchainCode/BlockTools.py
+++ si486i/blockchainCode/BlockTools.py
@@ -172,7 +172,7 @@ def send_chat(msg,host,port):
     # Send it
     printc(f"\tSending block to {host}",TAN)
     try:
-        post = http_post(URL['push'],push_data)
+        post = http_post(URL['push'],payload=push_data)
         if post == "200":
             printc(f"\tBlock sent successfully",GREEN)
         else:
