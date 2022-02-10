a28363a41233b2a5bc971ad3057895401e80c270
Steinshark <everettperson@gmail.com>
2022-02-09 23:54:02 -0500

    fix err

--- si486i/blockchainCode/BlockTools.py
+++ si486i/blockchainCode/BlockTools.py
@@ -175,6 +175,6 @@ def send_chat(msg,host,port):
         if post == 200:
             printc(f"\tBlock sent successfully",GREEN)
         else:
-            printc(f"\tCode recieved: {post}, data recieved: {data}",TAN)
+            printc(f"\tCode recieved: {post}",TAN)
     except TypeError:
         printc(f"\tRecieved Null response...",TAN)
