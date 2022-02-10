87a1cb2323b6e7b72d245a03123b9bcc05ae105e
Everett Stenberg <m226252@usna.edu>
2022-02-10 09:39:54 -0500

    err

--- si486i/blockchainCode/ServerSource.py
+++ si486i/blockchainCode/ServerSource.py
@@ -275,7 +275,7 @@ class DynamicServer:
             self.longest_chain = info['length']
             self.head_hash = info['head']
             flock(file,LOCK_UN)
-        printc(f"\t\thead is now at {self.head_hash} of len {self.longest_chain}")
+        printc(f"\t\thead is now at {self.head_hash} of len {self.longest_chain}", GREEN)
         self.all_chains = hash_len
 
     def update_chains(self,block):
