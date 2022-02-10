018495834d25f7d5e4c8b31b4f3cb2178b646fec
Steinshark <everettperson@gmail.com>
2022-02-09 22:35:51 -0500

    rework verbose output

--- si486i/blockchainCode/check_peers.py
+++ si486i/blockchainCode/check_peers.py
@@ -1,13 +1,22 @@
 from BlockchainUtilities import *
 from BlockchainErrors import *
 
+# Package import to work on windows and linux
+try:
+    sys.path.append("C:\classes")
+    sys.path.append("D:\classes")
+    from Toolchain.terminal import *
+except ModuleNotFoundError:
+    sys.path.append("/home/m226252/classes")
+    from Toolchain.terminal import *
+
+
 hosts = [line.strip() for line in open('hosts.txt').readlines()]
 ports = ["5000","5001","5002"]
 
 for host in hosts:
-    print(f"{Color.BLUE}attempting to access host: {host}{Color.END}")
+    printc(f"attempting conneciton to {host} on port {port}",BLUE)
     for port in ports:
-        print(f"{Color.TAN}on port {port}{Color.END}")
         try:
             blockchain_download = get_blockchain(host,port)
             size = len(blockchain_download)
