a8f06443f31209571c9ee9dee5eb54b26f0ef9bc
Steinshark <everettperson@gmail.com>
2022-02-09 23:09:05 -0500

    asdf

--- si486i/blockchainCode/BlockTools.py
+++ si486i/blockchainCode/BlockTools.py
@@ -132,7 +132,7 @@ def check_fields(block,allowed_versions=[0],allowed_hashes=[''],trust=False):
     elif (not 'prev_hash' in block) or\
          (not block['prev_hash'] in allowed_hashes):
         print(block['prev_hash'] in allowed_hashes)
-        print(f"type hash {type(block['prev_hash'])} type allowed {allowed_hashes[0]}")
+        print(f"type hash {type(block['prev_hash'])} type allowed {type(allowed_hashes[3])}")
         input()
         print(allowed_hashes)
         input()
