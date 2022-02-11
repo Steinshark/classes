e741b9c27b0abf577baa5600fb70cea98d47d169
Everett Stenberg <m226252@usna.edu>
2022-02-11 11:29:16 -0500

    caps

--- si470/movies/GPUexoptimized.py
+++ si470/movies/GPUexoptimized.py
@@ -10,13 +10,13 @@ import pprint
 
 times = {'start' : time()}
 # Package import to work on windows and linux
-sys.path.append("C:\classes\")
+sys.path.append("C:\classes")
 sys.path.append("D:\classes")
 sys.path.append("/mnt/d/classes")
 sys.stderr = sys.stdout
 print(sys.path)
 from Toolchain.terminal import *
-from Toolchain.GPUTools import *
+from Toolchain.gpuTools import *
 
 
 
