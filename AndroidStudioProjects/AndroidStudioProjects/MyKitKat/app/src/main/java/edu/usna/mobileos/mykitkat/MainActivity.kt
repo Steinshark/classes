55d52614056f95a24890ef4b5128a4cefce5b783
Steinshark <everettperson@gmail.com>
2022-02-14 17:42:29 -0500

    udpated project

--- AndroidStudioProjects/AndroidStudioProjects/MyKitKat/app/src/main/java/edu/usna/mobileos/mykitkat/MainActivity.kt
+++ AndroidStudioProjects/AndroidStudioProjects/MyKitKat/app/src/main/java/edu/usna/mobileos/mykitkat/MainActivity.kt
@@ -2,16 +2,18 @@ package edu.usna.mobileos.mykitkat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.ASSERT
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
 
-    lateinit var result: TextView
-    lateinit var button4: Button
+
+class MainActivity : AppCompatActivity() {
+     var outText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        result = findViewById(R.id.outputText)
     }
 }
