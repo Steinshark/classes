e766e0dd6abac436c071b5a5097159de3fd5c19c
Everett Stenberg <m226252@usna.edu>
2022-02-16 11:02:29 -0500

    algs update

--- si470/GradientDescent/Algorithms.py
+++ si470/GradientDescent/Algorithms.py
@@ -1,29 +1,32 @@
 import tensorflow as tf
 import numpy as np
 def GradientDescent(real_data,alpha=.1,iters=1000):
+
     # Ascertain what dimensions we are in
     cols    = real_data.shape[0]
     rows    = real_data.shape[1]
+
     # Create the P and Q matrices
     dim = 3
     p_init   = tf.random.uniform(shape = [cols,dim],dtype=tf.dtypes.float32)
     q_init   = tf.random.uniform(shape = [dim,rows],dtype=tf.dtypes.float32)
     print(p_init)
     print(q_init)
-    #      P                      Q
-    #[  .  .  .  ]      [  .  .  .  .  .  ]
-    #[  .  .  .  ]      [  .  .  .  .  .  ]
-    #[  .  .  .  ]      [  .  .  .  .  .  ]
-    #[  .  .  .  ]
-    #[  .  .  .  ]
-
+    return p_init,q_init
 
 # A is assumed to be the unknown values
-def RMSE(A,T):
-
+def RMSE(A,T,p,q):
 #       sqrt( (1 / T)*(sum(A_ij - (p_i)(q_j))^2) )
+    # 1 / T
+    printc(f"T is shape {A.shape}")
+    Tinv = tf.reciprocal(tf.constant([A.shape[0])],dtype=tf.dtypes.float32))
+    # p,q
+    pq = tf.matmul(p,q)
+    A_pq = tf.math.subtract(A,pq)
+    A_pq_square = tf.square(A_pq)
+    sum = tf.reduce_sum(A_pq_square)
+    return tf.sqrt(sum)
 
 
-    Tinv = tf.reciprocal(tf.norm(T))
-    A_sum_err =
-    return tf.sqrt(tf.matmul())
+if __name__ == "__main__":
+    a = tf.constant([[1,2,3],[4,5,6]],dtype=tf.dtypes.float32)
