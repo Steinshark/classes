adc3aa2e23c55e02a17e970233b443c69182cf42
Steinshark <everettperson@gmail.com>
2022-02-16 08:37:34 -0500

    imoport err

--- si486i/blockchainCode/send_chat.py
+++ si486i/blockchainCode/send_chat.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     n = Node()
     try:
         if sys.argv[1] == 'c':
-            send_chat(input("msg: "), input("host: "), 5002)
+            BlockTools.send_chat(input("msg: "), input("host: "), 5002)
             n.update_peers()
     except IndexError:
         msg = input("msg: ")
