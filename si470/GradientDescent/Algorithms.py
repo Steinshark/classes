9f717fa5778c12529960d34b9ef07f4e31c5c77f
Everett Stenberg <m226252@usna.edu>
2022-02-18 11:28:14 -0500

    from lab

--- si470/GradientDescent/Algorithms.py
+++ si470/GradientDescent/Algorithms.py
@@ -1,43 +1,68 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import numpy as np
 import math
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 
-def map_to_missing(holes, full):
-    d = tf.sparse.map_values(tf.ones_like,s)
+# Filters pq to only hold values that exist in the original ratings matrix
+# Filter is in form:
+
+#           [ 0  1  0  0  1 ]
+#           [ 0  1  0  1  0 ]
+#           [ 1  0  1  0  1 ]
+
+# Where 1 held a rating in the original dataset
+def map_to_existing(pq, filter):
+    res = tf.math.multiply(pq,filter)
+    input(res)
+    return tf.math.multiply(pq,filter)
 
 
-def GradientDescent(real_data,alpha=.1,iters=1000):
+def GradientDescent(A,filter,dim=10,alpha=.1,iters=1000):
 
     # Ascertain what dimensions we are in
-    cols    = real_data.shape[0]
-    rows    = real_data.shape[1]
+    cols    = A.shape[0]
+    rows    = A.shape[1]
 
     # Create the P and Q matrices
-    dim = 3
-    p_init   = tf.random.uniform(shape = [cols,dim],dtype=tf.dtypes.float32)
-    q_init   = tf.random.uniform(shape = [dim,rows],dtype=tf.dtypes.float32)
+    p       = tf.random.uniform(shape = [cols,dim],dtype=tf.dtypes.float32)
+    q       = tf.random.uniform(shape = [dim,rows],dtype=tf.dtypes.float32)
+
+    for i in range(iters):
+        pq      = tf.matmul(p,q)
+        A_pq   = tf.math.subtract(A,pq)
+
+        # Calculate new p and q
+        additional_p = tf.math.subtract(tf.transpose(q), A_pq) 
+        new_p      = tf.math.add(p,    tf.math.multiply(    tf.math.multiply(  (2.0 * alpha), tf.transpose(q)),  A_pq))
+        new_q      = tf.math.add(p,    tf.math.multiply(    tf.math.multiply(  (2.0 * alpha), tf.transpose(p)),  A_pq))
+
+        p = new_p
+        q = new_q
+
+        input(RMSE(A,filter,p,q))
     return p_init,q_init
 
 # A is assumed to be the Sparse Matrix of Ratings with many holes
-def RMSE(A,p,q):
-#       sqrt( (1 / T)*(sum(A_ij - (p_i)(q_j))^2) )
-    # 1 / T
-    Tinv = tf.constant(1 / tf.math.count_nonzero(A),dtype=tf.dtypes.float32)
+def RMSE(A, filter,p,q):
 
-    #
-    pq = tf.matmul(p,q)
+    # 1 / T
+    Tinv = tf.math.count_nonzero(filter,dtype=tf.dtypes.float32)
+
+    # build pq with only elements that exist in A
+    pq = map_to_existing(tf.matmul(p,q),filter)
+
+    # Find distances squared from A
     A_pq = tf.math.subtract(A,pq)
     A_pq_square = tf.square(A_pq)
 
-    # Dont incldue blank indices 'Frobenius norm '
+    # Find sum of distances
     sum = tf.reduce_sum(A_pq_square)
 
-
+    # Return root of distances
     return math.sqrt(sum * Tinv)
 
 
--- si470/GradientDescent/test.py
+++ si470/GradientDescent/test.py
@@ -1,83 +1,74 @@
+import tensorflow as tf
 import pandas as pd
-from os.path import join
 import numpy as np
 import os
-import sys
-from time import time
-import pprint
-from sklearn.decomposition import TruncatedSVD
-import scipy
+import Algorithms
+
+#TYPES
+_f32            = tf.dtypes.float32
+
+#DATASET LOCATIONS
+_RATINGS_SMALL  = os.path.join("ml-latest-small","ratings.csv")
+_MOVIE_ID_SMALL = os.path.join("ml-latest-small","movies.csv")
+_HEADERS        = ["Black Panther",
+                   "Pitch Perfect",
+                   "Star Wars: The Last Jedi",
+                   "It",
+                   "The Big Sick",
+                   "Lady Bird",
+                   "Pirates of the Caribbean",
+                   "Despicable Me",
+                   "Coco",
+                   "John Wick",
+                   "Mamma Mia",
+                   "Crazy Rich Asians",
+                   "Three Billboards Outside Ebbings, Missouri",
+                   "The Incredibles"]
+
+_MOVIE_ID_MAP      = {}
+_ROW_MAP           = {}
 
 
-
-def load_data():
-    self.datasets = {
-                    'small'     :  {'movies'        : join('ml-latest-small','movies.csv') , 'ratings' : join("ml-latest-small","ratings.csv") , 'tags' : join("ml-latest-small","tags.csv")},
-                    'large'     :  {'movies'        : join("ml-latest","movies.csv")       , 'ratings' : load_from[self.input_source]       , 'tags' : join('ml-latest',"tags.csv")},
-                    'usna'      :  {'foodMovies'    : 'foodAndMovies.csv'}}                                #join("ml-latest","ratings.csv")
-
-    self.dataframes= {
-                    'small'     :  {'movies'        : None, 'ratings' :  None, 'tags' : None},
-                    'large'     :  {'movies'        : None, 'ratings' :  None, 'tags' : None}}
-
-
-    self.headers = ["Black Panther","Pitch Perfect","Star Wars: The Last Jedi","It","The Big Sick","Lady Bird","Pirates of the Caribbean","Despicable Me","Coco","John Wick","Mamma Mia","Crazy Rich Asians","Three Billboards Outside Ebbings, Missouri","The Incredibles"]
-
-def read_data(self):
-
-    # Read in all CSVs to DataFrames
-    printc(f"\nBEGIN: Data read from CSV",BLUE,endl='')
-    t1 = time()
-    for size in self.datasets:
-        for dset in self.datasets[size]:
-            if not (size == 'large' and dset == 'ratings') and not (size == 'usna' and dset == 'foodMovies'):
-                # Read and print status
-                printc(f"\n\treading {dset}-{size}",TAN,endl='')
-                self.dataframes[size][dset]   = pd.read_csv(self.datasets[size][dset],sep=',')
-                printc(f"\n\tsize: {(self.dataframes[size][dset].memory_usage().sum() / (1024.0*1024.0)):.2f} MB",TAN,endl='')
-
-    # Read larger 'ratings' dataset
-    printc(f"\n\treading ratings-large",TAN)
-    self.ratings        = pd.read_csv(  self.datasets['large']['ratings'],
-                                        dtype   =   {   'userId'    :   np.float32,
-                                                        'movieId'   :   np.float32,
-                                                        'rating'    :   np.float64,
-                                                        'timestamp' :   np.float64},
-                                        sep     =   ',')
-    printc(f"\tsize: {(self.ratings.memory_usage().sum() / (1024.0*1024.0)):.2f} MB",TAN)
-
-    # Gather the USNA data and prepare it
-    self.usna           = pd.read_csv(  self.datasets['usna']['foodMovies'],     sep = ',')[self.headers]
-
-    # Fix USNA data with MEAN
-    for col in self.headers:
-        if self.replace     == 'mean':
-            replacement = self.usna[col].mean()
-        elif self.replace   == 'zero':
+def fill(df,method):
+    for col in df:
+        if method == 'mean':
+            replacement = df[col].mean()
+        elif method   == 'zero':
             replacement = 0
-        self.usna[col].fillna(value = 0, inplace = True)
+        df[col].fillna(value = replacement, inplace = True)
 
-    # Some helpful constants                                  dataset                             column                index
-    self.n_movies                            = int( len(    self.dataframes['large']['movies']     ['movieId']                     ))
-    self.n_users                             = int(         self.ratings                           ['userId'].iloc     [-1]        )
 
-    # Fix the mapping of movieId to col in matrix
-    self.movieId_to_col = {}
-    self.movieId_to_name = {}
-    self.col_to_movieId = {}
-    i = 0
-    with open(self.datasets['large']['movies'],'r',encoding='utf-8') as file:
-        file.readline()
-        for line in file:
-            movieID = int(line.split(',')[0])
-            movieName = line.split(',')[1]
-            self.movieId_to_col[movieID] = i
-            self.movieId_to_name[movieID] = movieName
-            self.col_to_movieId[i] = movieID
-            i += 1
+def read_data():
+    # Read 'ratings.csv' into a DataFrame from the small set
+    small_ratings   = pd.read_csv(  _RATINGS_SMALL,
+                                    dtype = {   'userId'    :   np.float32,
+                                                'movieId'   :   np.float32,
+                                                'rating'    :   np.float32,
+                                                'timestamp' :   np.float32})
 
-    self.liked_movies = list(map(lambda x : self.movieId_to_col[x], self.liked_movies))
+    small_movie_ids  = pd.read_csv(  _MOVIE_ID_SMALL,
+                                    dtype = {   'movieId' : np.float32})
 
-    # Done with data read!
-    printc(f"Read Data in {(time()-t1):.3f} seconds",GREEN)
-    printc(f"Expecting to build matrix size ({self.n_movies},{self.n_users})",TAN)
+    # Convert the dataframe to correct matrix format: (rows=MovieId, cols=userId)
+    small_ratings_matrix = small_ratings.pivot( index='movieId',    columns='userId',   values='rating')
+
+    # Map movieId to row and vice versa
+    for row, id in enumerate(small_movie_ids['movieId']):
+        _MOVIE_ID_MAP[row]  = id
+        _ROW_MAP[id]        = row
+
+    #info
+    # Fill with our choice
+    fill(small_ratings_matrix,'zero')
+
+    # convert the dataframe to a Tensor
+    partial_ratings_tensor  = tf.convert_to_tensor(  small_ratings_matrix,   dtype = _f32)
+
+    # convert all non_zero elements to 1
+    partial_ratings_filter  = tf.sparse.to_dense(tf.sparse.map_values(tf.ones_like,tf.sparse.from_dense(partial_ratings_tensor)))
+    partial_ratings_filter  = tf.cast(partial_ratings_filter, dtype=_f32)
+
+    return partial_ratings_tensor, partial_ratings_filter
+
+ratings, filter = read_data()
+Algorithms.GradientDescent(ratings,filter,dim=10)
